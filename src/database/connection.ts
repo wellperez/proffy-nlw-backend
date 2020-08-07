3bd3a9123aef3242d61ae93513f80297e9ba45e6
Wellington Perez <wellingtonx3@hotmail.com>
2020-08-07 09:56:11 -0300

    Add routes file and classes post route

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,9 +2,9 @@ import knex from 'knex'
 import path from 'path'
 
 const db = knex({
-  client:'sqlite3',
+  client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, 'connection','database.sqlite')
+    filename: path.resolve(__dirname,'database.sqlite')
   },
   useNullAsDefault:true,
 });
