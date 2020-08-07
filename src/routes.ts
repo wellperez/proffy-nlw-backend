92b4294316aaaf36aafa887a4b1cb33e6ef617e0
Wellington Perez <wellingtonx3@hotmail.com>
2020-08-07 10:59:42 -0300

    Add class schedule function

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,14 @@
 import express, { response } from 'express'
 import db from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
 
 const routes = express.Router();
+
+interface ScheduleItem{
+  week_day: number;
+  from: string;
+  to: string; 
+}
 
 routes.post('/classes', async (request,response) => {
   const {
@@ -14,12 +21,37 @@ routes.post('/classes', async (request,response) => {
     schedule
   } = request.body;
 
-  await db('users').insert({
+  const trx = await db.transaction();
+
+  const insertedUsersIds = await trx('users').insert({
     name,
     avatar,
     whatsapp,
     bio,
   })
+  
+  const user_id = insertedUsersIds[0];
+
+  const insertedClassesIds = await trx('classes').insert({
+    subject,
+    cost,
+    user_id,
+  })
+
+  const class_id = insertedClassesIds[0];
+  
+  const classSchedule = schedule.map((element: ScheduleItem) => {
+    return {
+      week_day: element.week_day,
+      from: convertHourToMinutes(element.from),
+      to: convertHourToMinutes(element.to),
+      class_id
+    }
+  });
+  
+  await trx('class_schedule').insert(classSchedule);
+  
+  await trx.commit();
 
   return response.send()
 })
