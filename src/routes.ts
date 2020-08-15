cdf3e58ab87e3f19c92dcbb4ea3ae1900eb8d3b1
Wellington Perez <wellingtonaperez@gmail.com>
2020-08-15 20:30:00 -0300

    Add index total connections and cors

--- src/routes.ts
+++ src/routes.ts
@@ -10,6 +10,7 @@ const connectionsControllers = new ConnectionsController();
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
 
+routes.get('/connections', connectionsControllers.index);
 routes.post('/connections', connectionsControllers.create);
 
 export default routes;
